42b6475a5dbcf15593a7ae39b1a8ab007005cbf9
Shams Ali <ali.s90@outlook.com>
2017-01-08 01:11:46 -0600

    (add) mailer for chat logs

--- app/controllers/v1/mailer_controller.js
+++ app/controllers/v1/mailer_controller.js
@@ -3,6 +3,8 @@
 const Nodal = require('nodal');
 const mandrill = require('node-mandrill')(process.env.MANDRILL_API);
 const User = Nodal.require('app/models/user.js');
+const Message = Nodal.require('app/models/message.js');
+const _ = require('underscore');
 
 class V1MailerController extends Nodal.Controller {
 
@@ -17,35 +19,61 @@ class V1MailerController extends Nodal.Controller {
     const that = this;
     let text = '', subject= '', message = {};
     console.log('post in mailer hit');
-    User.find(this.params.route.id, (err, model) => {
-      if (this.params.query.type === 'newChat') {
+    if (this.params.query.type === 'newChat') {
+      User.find(this.params.route.id, (err, model) => {
         text = JSON.stringify(model._data);
         subject = `${model._data.first_name} Just Entered A Chat!`;
-      }
-      // TODO: change email and name
-      message.message = {
-        to: [{email: 'ali@operationspark.org', name: 'Shams Ali'}],
-        from_email: model._data.email,
-        subject: subject,
-        text: text
-      };
+        // TODO: change email and name
+        message.message = {
+          to: [{email: 'ali@operationspark.org', name: 'Shams Ali'}],
+          from_email: 'ali@operationspark.org',
+          subject: subject,
+          text: text
+        };
 
-      mandrill('/messages/send', message, function(error, response){
-        console.log('mandrill hit');
-        if (error){
-          error = JSON.stringify(error)
-          console.log(error);
-          that.respond({message: error})
-        } else if (response[0].status === "rejected"){
-          console.log(response)
-          that.respond({error: response});
-        } else {
-          that.respond({message: response});
-        }
+        mandrill('/messages/send', message, function(error, response){
+          console.log('mandrill hit');
+          if (error){
+            error = JSON.stringify(error)
+            console.log(error);
+            that.respond({message: error})
+          } else if (response[0].status === "rejected"){
+            console.log(response)
+            that.respond({error: response});
+          } else {
+            that.respond({message: response});
+          }
+        });
+        console.log('after mandrill');
+
       });
-      console.log('after mandrill');
+    } else if (this.params.query.room_id) {
 
-    });
+      Message.query()
+        .where(this.params.query)
+        .end((err, models) => {
+          // console.log(models[0])
+          text = JSON.stringify(_.reduce(models, function(prev, curr){
+            return prev.concat({
+              from_username: curr._data.from_username,
+              body: curr._data.body
+            });
+          }, []));
+        subject = 'Chat Logs';
+        // TODO: change email and name
+        message.message = {
+          to: [{email: 'ali@operationspark.org', name: 'Shams Ali'}],
+          from_email: 'ali@operationspark.org',
+          subject: subject,
+          text: text
+        };
+
+        this.respond(err || models);
+
+      });
+
+    }
+
 
 
   }
